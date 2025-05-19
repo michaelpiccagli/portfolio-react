56fa5258c2e40deac05c3fc3445b7912d7bfbb73
Michael Piccagli <michaelp_@hotmail.com>
2025-05-19 13:25:56 -0300

    import Skills corrigido

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './styles/animations.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
-import Skills from './components/skills';
+import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import TopButton from './components/TopButton';
